e9237742bdf725153e0804d1f844efc90e3bf4f6
Balaji <balajikaredla12345@gmail.com>
2024-01-21 19:24:25 +0530

    Responsive-1

--- script.js
+++ script.js
@@ -28,17 +28,6 @@ document.querySelectorAll('.nav-link').forEach(() => {
 	this.addEventListener('mouseleave', e => element.classList.remove(hover))
 })
 
-// function cli(value) {
-// 	for (i=0; i<5; i++) {
-// 		console.log(i, value, i==value)
-// 		if (i == value) {
-// 			document.querySelector(`#page${i}`).style.display = 'flex'
-// 		} else {
-// 			document.querySelector(`#page${i}`).style.display = 'none'
-// 		}
-// 	}
-// }
-
 document.querySelectorAll('.nav-link').forEach(link => {
 	value = link.dataset.value
 	link.onclick = function() {
@@ -76,17 +65,3 @@ document.querySelectorAll('.nav-link-mobile').forEach(link => {
 		document.querySelector('#scroll-menu').style.display = 'none'
 	}
 })
-// for (j=0; j<5; j++){
-// 	link = links[j]
-// 	value = link.dataset.value
-// 	link.onclick = () => {
-// 		for (i=0; i<5; i++) {
-// 			console.log(this)
-// 			if (i == value) {
-// 				document.querySelector(`#page${i}`).style.display = 'flex'
-// 			} else {
-// 				document.querySelector(`#page${i}`).style.display = 'none'
-// 			}
-// 		}
-// 	}
-// }
